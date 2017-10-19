3444f06e6bb2017d166e966da972af10f4954179
Nickolay Makhyboroda <dev.nimblemind@gmail.com>
2017-10-19 21:34:21 +0200

    Добавил возможность снимать фото запчастей

--- app/src/main/java/com/nimblemind/autoplus/NewRequestActivity.java
+++ app/src/main/java/com/nimblemind/autoplus/NewRequestActivity.java
@@ -1,24 +1,40 @@
 package com.nimblemind.autoplus;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.bumptech.glide.Glide;
 
 import java.util.Calendar;
 
 
 public class NewRequestActivity extends AppCompatActivity
 {
+	private static final int RC_TAKE_PICTURE = 105;
+
+	// permission request codes need to be < 256
+	private static final int CODE_REQUEST_CAMERA = 201;
+
 	private TextView nameView;
 	private TextView vinView;
 	private TextView yearView;
 	private TextView engineView;
 	private TextView partView;
 	private TextView commentView;
+	private ImageView imageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -32,6 +48,7 @@ public class NewRequestActivity extends AppCompatActivity
 		engineView = findViewById(R.id.textEngine);
 		partView = findViewById(R.id.textPart);
 		commentView = findViewById(R.id.textComment);
+		imageView = findViewById(R.id.image);
 
 		final String uid;
 		Intent intent = getIntent();
@@ -55,6 +72,107 @@ public class NewRequestActivity extends AppCompatActivity
 				submit(uid);
 			}
 		});
+
+		imageView.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				tryLaunchCamera();
+			}
+		});
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (resultCode == RESULT_OK)
+		{
+			if (requestCode == RC_TAKE_PICTURE)
+			{
+				Uri uri = data.getData();
+
+				Glide.with(this)
+						.load(uri)
+						.into(imageView);
+			}
+		}
+		else
+		{
+			if (requestCode == RC_TAKE_PICTURE)
+			{
+				Toast.makeText(this, "Taking picture failed.", Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+
+	private void tryLaunchCamera()
+	{
+		int rc = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA);
+		if (rc == PackageManager.PERMISSION_GRANTED)
+		{
+			launchCamera();
+		}
+		else
+		{
+			requestCameraPermission();
+		}
+	}
+
+	private void requestCameraPermission()
+	{
+		final String[] permissions = new String[] { android.Manifest.permission.CAMERA };
+
+		if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA))
+		{
+			ActivityCompat.requestPermissions(this, permissions, CODE_REQUEST_CAMERA);
+		}
+		else
+		{
+			new AlertDialog.Builder(this)
+					.setTitle(R.string.textRationaleTitle)
+					.setMessage(R.string.textRationaleMessageCamera)
+					.setPositiveButton(R.string.textOK, new DialogInterface.OnClickListener()
+					{
+						@Override
+						public void onClick(DialogInterface dialogInterface, int i)
+						{
+							ActivityCompat.requestPermissions(NewRequestActivity.this, permissions,
+									CODE_REQUEST_CAMERA);
+						}
+					})
+					.setNegativeButton(R.string.textClose, null)
+					.create()
+					.show();
+		}
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+	{
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+		if (requestCode == CODE_REQUEST_CAMERA
+				&& grantResults.length != 0
+				&& grantResults[0] == PackageManager.PERMISSION_GRANTED)
+		{
+			launchCamera();
+		}
+	}
+
+	private void launchCamera()
+	{
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(intent, RC_TAKE_PICTURE);
+	}
+
+	private void openGallery()
+	{
+		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+		intent.setType("image/*");
+		startActivityForResult(intent, RC_TAKE_PICTURE);
 	}
 
 	private void submit(String uid)
