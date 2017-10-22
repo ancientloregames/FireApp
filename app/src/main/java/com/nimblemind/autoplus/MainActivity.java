3621cb1583daff21a4207f19b27f3ed49a44a5e2
Nickolay Makhyboroda <dev.nimblemind@gmail.com>
2017-10-22 22:07:13 +0200

    Переделал main activity на drawer layout

--- app/src/main/java/com/nimblemind/autoplus/MainActivity.java
+++ app/src/main/java/com/nimblemind/autoplus/MainActivity.java
@@ -1,12 +1,18 @@
 package com.nimblemind.autoplus;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import com.google.firebase.auth.FirebaseAuth;
 
 
@@ -14,9 +20,16 @@ import com.google.firebase.auth.FirebaseAuth;
  * com.nimblemind.autoplus. Created by nimblemind on 10/4/2017.
  */
 
-public class MainActivity extends AppCompatActivity implements RequestsFragment.Listener
+public class MainActivity extends AppCompatActivity implements RequestsFragment.Listener,
+		NavigationView.OnNavigationItemSelectedListener
 {
 	private final String TAG = MainActivity.class.getSimpleName();
+
+	private DrawerLayout drawer;
+	private ActionBarDrawerToggle drawerToggle;
+
+	private String uid;
+	private UserType userType;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState)
@@ -27,50 +40,91 @@ public class MainActivity extends AppCompatActivity implements RequestsFragment.
 		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
-		String uid = getIntent().getStringExtra("uid");
+		drawer = findViewById(R.id.drawer);
+		drawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawerOpen, R.string.drawerClose);
+		drawer.addDrawerListener(drawerToggle);
+		drawerToggle.syncState();
+
+		NavigationView navigationView = findViewById(R.id.navigator);
+		navigationView.setNavigationItemSelectedListener(this);
+
+		uid = getIntent().getStringExtra("uid");
 		User user = (User) getIntent().getSerializableExtra("user");
+		userType = user.type;
 
 		Log.d(TAG, user.toString() + " uid: " + uid);
 
-		new AlertDialog.Builder(this)
-				.setTitle("Successful enter")
-				.setMessage("uid: " + uid + "\n" + user.toString())
-				.setPositiveButton("OK", null)
-				.create()
-				.show();
+//		new AlertDialog.Builder(this)
+//				.setTitle("Successful enter")
+//				.setMessage("uid: " + uid + "\n" + user.toString())
+//				.setPositiveButton("OK", null)
+//				.create()
+//				.show();
 
 		if (savedInstanceState == null)
 		{
-			onFirstCreate(uid, user);
+			onNavigationItemSelected(navigationView.getMenu().findItem(R.id.navTicketFragment));
 		}
 	}
 
-	private void onFirstCreate(String uid, User user)
+	@Override
+	public void onBackPressed()
+	{
+		if (drawer.isDrawerOpen(GravityCompat.START))
+		{
+			drawer.closeDrawer(GravityCompat.START);
+		}
+		else
+		{
+			super.onBackPressed();
+		}
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		drawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(@NonNull MenuItem item)
 	{
 		RequestsFragment fragment = null;
-
-		switch (user.type)
+		int id = item.getItemId();
+		switch (id)
 		{
-			case CLIENT:
-				fragment = new ClientTicketsFragment();
+			case R.id.navTicketFragment:
+				if (userType == UserType.CLIENT)
+				{
+					fragment = new ClientTicketsFragment();
+				}
+				else if (userType == UserType.SUPPORT)
+				{
+					// TODO
+				}
 				break;
-			case SUPPORT:
-				// TODO Сделать инфраструктуру для учетной записи сотрудника
+			case R.id.navOfferFragment:
+				// TODO
 				break;
-			default:
-				// NOTE Такого не может произойти, так ведь? Возвращаемся на форму авторизации
-				gotoAuthActivity(true);
-				return;
+			case R.id.navOrderFragment:
+				// TODO
+				break;
 		}
 
-		Bundle arguments = new Bundle();
-		arguments.putString("uid", uid);
-		fragment.setArguments(arguments);
+		drawer.closeDrawer(GravityCompat.START);
 
-		getSupportFragmentManager()
-				.beginTransaction()
-				.add(R.id.fragmentTarget, fragment)
-				.commitNowAllowingStateLoss();
+		if (fragment != null)
+		{
+			Bundle arguments = new Bundle();
+			arguments.putString("uid", uid);
+			fragment.setArguments(arguments);
+			getSupportFragmentManager()
+					.beginTransaction()
+					.replace(R.id.fragmentTarget, fragment)
+					.commitNowAllowingStateLoss();
+			return true;
+		}
+		else return super.onOptionsItemSelected(item);
 	}
 
 	private void gotoAuthActivity(final boolean noAutologin)
